2ba1494518ca4499844fe99fa799e3b0888fd35e
NATAKENSHI DEVELOPMENT <ptazzamcodex@gmail.com>
2025-06-17 14:07:07 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,13 @@
-aaa
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ESX AFK Detection and Teleport'
+author 'NatakenshiDev'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
